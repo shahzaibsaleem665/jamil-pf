13aa5e1d436843ac95bceedea5d5f4b0bf2e8337
shahzaib saleem <shahzaibsaleem665@gmail.com>
2024-05-25 22:16:29 +1000

    menu completed

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -2,29 +2,21 @@ import React, { useEffect, useState } from 'react';
 import white from '../assets/white.png';
 import './Header.css';
 import { useHistory, useLocation } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 function Header() {
   const [selectedButton, setSelectedButton] = useState('');
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const history = useHistory();
   const location = useLocation();
-  
-
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const listItems = ['Home', 'Publications', 'Research Work', 'About me', 'Contact me'];
 
   useEffect(() => {
-    // Extract the selected button from the pathname
     const selected = location.pathname.split('/')[1];
     setSelectedButton(selected);
   }, [location]);
-
-  const handleButtonClick = (item) => {
-    const formattedItem = item.toLowerCase().replace(/\s+/g, '-');
-    setSelectedButton(formattedItem);
-    history.push(`/${formattedItem}`);
-  };
-
-
 
   useEffect(() => {
     const handleResize = () => {
@@ -38,32 +30,63 @@ function Header() {
     };
   }, []);
 
-
   const homeNav = (e) => {
     e.preventDefault();
     history.push('/');
-  }
+    closeMenu();
+  };
 
+  const handleButtonClick = (item) => {
+    const formattedItem = item.toLowerCase().replace(/\s+/g, '-');
+    setSelectedButton(formattedItem);
+    history.push(`/${formattedItem}`);
+    closeMenu();
+  };
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
 
   return (
-    <div className='header'>
-        <div className="header__left">
-            <img src={white} alt='logo' onClick={homeNav}/>
-        </div>
-        
-        <div className="header__right">
-          {listItems.map((item, index) => (
-            <p 
-              key={item} 
-              className={selectedButton === item.toLowerCase().replace(/\s+/g, '-') ? 'active' : ''} 
-              onClick={() => handleButtonClick(item)}
-            >
-              {item}
-            </p>
-          ))}
-        </div>
+    <div className="header">
+    <div className="header__left">
+      <img src={white} alt="logo" onClick={homeNav} />
     </div>
+    <div className="header__right">
+      {windowWidth < 576 ? (
+        <div className="mobile__menu">
+          <div className="menu__icon" onClick={toggleMenu}> {isMenuOpen ? <CloseIcon /> : <MenuIcon />}</div>
+          {isMenuOpen && (
+            <div className="mobile__menuItems">
+              {listItems.map((item) => (
+                <p
+                  key={item}
+                  className={selectedButton === item.toLowerCase().replace(/\s+/g, '-') ? 'active' : ''}
+                  onClick={() => handleButtonClick(item)}
+                >
+                  {item}
+                </p>
+              ))}
+            </div>
+          )}
+        </div>
+      ) : (
+        listItems.map((item) => (
+          <p
+            key={item}
+            className={selectedButton === item.toLowerCase().replace(/\s+/g, '-') ? 'active' : ''}
+            onClick={() => handleButtonClick(item)}
+          >
+            {item}
+          </p>
+        ))
+      )}
+    </div>
+  </div>
   );
 }
 
